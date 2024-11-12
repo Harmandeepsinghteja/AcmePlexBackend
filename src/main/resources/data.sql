4c6580db6545a96b2b80490679296e2bb640766e
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2024-11-12 12:31:36 -0700

    Data.sql updated with dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-
+CREATE DATABASE IF NOT EXISTS test;
+USE test;
 INSERT INTO movie (id,title, added_date,url) 
 VALUES (1,'Interstellar Public','2024-08-24','https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/45fc99105415493.619ded0619991.jpg');
 
@@ -16,3 +17,18 @@ VALUES (4,'Inception Public','2024-11-01','https://flxt.tmsimg.com/assets/p78256
 
 INSERT INTO movie (id,title, added_date,url) 
 VALUES (5,'The Dark Knight Rises Public','2024-11-01','https://th.bing.com/th/id/OIP.ILrjvLryU-PMd7Cl7Yh7QQHaK-?rs=1&pid=ImgDetMain');
+
+
+INSERT INTO schedule (id, movie_id, screen_id, start_time, price)
+VALUES (1,1,1,'2024-11-01 10:00:00', 10.0);
+
+INSERT INTO schedule (id, movie_id, screen_id, start_time, price)
+VALUES (2,1,1,'2024-11-01 14:00:00', 10.0);
+
+
+INSERT INTO schedule (id, movie_id, screen_id, start_time, price)
+VALUES (3,1,1,'2024-11-01 17:00:00', 10.0);
+
+
+INSERT INTO screen (id, screen_name, length,wide,capacity)
+VALUES (1,'Screen 1', 5,10,50);
