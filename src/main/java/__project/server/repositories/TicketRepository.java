cec44eb7a6563652386f2ce5541c62ba373c7d31
aymanshahriar <ayman.shahriar@ucalgary.ca>
2024-11-17 19:26:33 -0700

    Implement rest of the endpoints for ticket

--- src/main/java/__project/server/repositories/TicketRepository.java
+++ src/main/java/__project/server/repositories/TicketRepository.java
@@ -1,10 +1,28 @@
 package __project.server.repositories;
 
+import __project.server.Entity.ReservationDetails;
 import __project.server.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
+    @Query(value = "" +
+            """
+            SELECT ticket.id as ticketId, movie.movieName as movieName, screen.screenName as screenNumber, ticket.seatNumber, schedule.startTime
+            FROM ticket
+            JOIN schedule on schedule.id = ticket.scheduleId
+            JOIN movie on movie.id = schedule.movieId
+            JOIN screen on screen.id = schedule.screenId
+            WHERE ticket.userId = ?1 AND schedule.startTime >= NOW() AND ticket.isCancelled = FALSE;
+            """
+            , nativeQuery = true)
+    public List<ReservationDetails> findUpcomingReservedTickets(int userId);
+
+    //public Optional<Ticket> findFirst1ByIdAndUserId(int ticketId, int userId);
 }
