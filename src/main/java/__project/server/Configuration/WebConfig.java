b2a7939a3d98460b33061ed87f5944a9ef56a4e5
aymanshahriar <ayman.shahriar@ucalgary.ca>
2024-11-18 14:54:03 -0700

    Update web config

--- src/main/java/__project/server/Configuration/WebConfig.java
+++ src/main/java/__project/server/Configuration/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
