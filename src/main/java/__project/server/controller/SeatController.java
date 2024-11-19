f2d8fdee6e783ce1eb885ba36ee6fd305c5f9560
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2024-11-18 19:14:36 -0700

    Adding Header file

--- src/main/java/__project/server/controller/SeatController.java
+++ src/main/java/__project/server/controller/SeatController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestHeader;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
